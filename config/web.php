37f781d020700e828e59d02567ec232d7e669424
vaguiiinho <39246197+vaguiiinho@users.noreply.github.com>
2018-08-27 17:46:17 -0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -75,7 +75,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['127.0.0.1', '10.0.2.2'],
+        //'allowedIPs' => ['127.0.0.1'],
     ];
 }
 
